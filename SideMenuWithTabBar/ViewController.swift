7520adaaffc6b1b27d23793993da8e179494a256
Davit <hovsepyan.davit93@gmail.com>
2022-05-03 20:48:28 +0400

    Initial Commit

---
+++ SideMenuWithTabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenuWithTabBar
+//
+//  Created by Davit Hovsepyan on 03.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
